c111bfbd7e5a1d7b6589c1cf8ae2759c6c0b3323
rodrigo-Dz <rodrigo.agdz@gmail.com>
2026-01-20 12:49:47 -0600

    Add files via upload

---
+++ Sequestration.jl
@@ -0,0 +1,40 @@
+# IFFL sequestration based model
+
+module mm
+	# Required libraries
+	using DifferentialEquations
+	using ParameterizedFunctions
+    using OrderedCollections  
+      
+    # ODE system with feedback
+    function IFFL(du, u, p, t)
+        U1, U2, X, C = u
+        m1, m2, mx, g, d, g0, f, A = p
+    
+        if f != 0
+            Y = A*(sin(2*pi*t*f) + 1)
+        else
+            Y = A
+        end
+    
+        du[1] = m1*Y - d*U1 - g*U1*U2 + g0*C
+        du[2] = m2*X - d*U2 - g*U1*U2 + g0*C
+        du[3] = mx*Y - d*X
+        du[4] = g*U1*U2 - g0*C - d*C
+    end
+
+    # Parameters 
+    p = OrderedDict(
+    :m1 => 1.0,
+    :m2 => 1.0,
+    :mx => 1.0, 
+    :g => 100.0, 
+    :d => 1.0,
+    :g0 => 0.001,
+    :f => 0.5,
+    :A => 5.0
+    )
+ 
+
+    u0 = [0.0, 0.0, 0.0, 0.0]     # Initial conditions for the ODE system
+end
